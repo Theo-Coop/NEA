54359bb683b79440ea78408680d72a716ddef0df
Theo-Coop <theomantelcooper@gmail.com>
2022-10-12 17:57:12 +0100

    continue gui

--- Sudoku/gui_test.py
+++ Sudoku/gui_test.py
@@ -6,13 +6,17 @@ class Gui:
         self.window = Tk()
         self.window.title("Sudoku")
 
-        for row in range(1, 10):
-            for column in range(1, 10):
+        self.button_dict = {}
+        self.num_button_dict = {}
+
+        for column in range(1, 10):
+            for row in range(1, 10):
 
                 if row in (1,2,3,7,8,9) and column in (4,5,6) or row in (4,5,6) and column in (1,2,3,7,8,9):
-                    colour = "red"
+                    colour = "#757070"
                 else:
-                    colour = "blue"
+                    colour = "#969494"
+                    
 
                 frame = Frame(self.window, width=10, height=10, padx=5, pady=5, bg=colour)
                 frame.grid(row=row, column=column)
@@ -25,11 +29,20 @@ class Gui:
                 button = Button(frame, justify="center", width=4, height=2)
                 button.pack()
 
+                self.button_dict[(column, row)] = button
+
         for i in range(1, 10):
-            new_button = Button(self.window, width=4, height=2, text=i)
-            new_button.grid(row=10, column=i)
+            num_button = Button(self.window, width=4, height=2, text=i, command=lambda: self.update_buttons(i))
+            num_button.grid(row=10, column=i)
+
+            self.num_button_dict[i] = num_button
+
+        print(self.button_dict)
 
         self.window.mainloop()
+
+    def update_buttons(self, num):
+        print(num)
 
 if __name__ == "__main__":
     Gui()
--- Sudoku/test.py
+++ Sudoku/test.py
@@ -1,5 +1,7 @@
 cells = {}
 
-cells[(1, 2)] = "hello"
+for i in range(1, 10):
+    for j in range(1, 10):
+        cells[(i, j)] = "hello"
 
 print(cells)
