facaa356315b83f780c254c309a307ce22d540aa
Theo-Coop <theomantelcooper@gmail.com>
2022-10-13 14:17:18 +0100

    starting nums working

--- Sudoku/gui_test.py
+++ Sudoku/gui_test.py
@@ -6,16 +6,20 @@ class Gui:
         self.window = Tk()
         self.window.title("Sudoku")
 
-        self.button_dict = {}
+        self.game_button_dict = {}
+        self.starting_button_dict = {}
         self.num_button_dict = {}
 
-        for column in range(1, 10):
-            for row in range(1, 10):
+        self.board = Board()
+        
+
+        for row in range(1, 10):
+            for column in range(1, 10):
 
                 if row in (1,2,3,7,8,9) and column in (4,5,6) or row in (4,5,6) and column in (1,2,3,7,8,9):
-                    colour = "#757070"
+                    colour = "#FF99D7"
                 else:
-                    colour = "#969494"
+                    colour = "#FFD372"
                     
 
                 frame = Frame(self.window, width=10, height=10, padx=5, pady=5, bg=colour)
@@ -25,15 +29,29 @@ class Gui:
                 # cell.pack()
 
                 # Still unsure whether to use buttons or text-entries
+                start_text = self.board.get_num(row, column)
+                if start_text == 0:
+                    game_buttons = Button(frame, justify="center", width=4, height=2, padx=0, pady=0, foreground="red", font=("Arial", 12, "bold"), command=lambda row=row, col=column: self.update_num(row, col))
+                    game_buttons.pack()
+                    self.game_button_dict[(row, column)] = game_buttons
 
-                button = Button(frame, justify="center", width=4, height=2, padx=0, pady=0, font=("Arial", 12, "bold"), command=lambda col=column, row=row: self.update_num(col, row))
-                button.pack()
+                else:
+                    starting_buttons = Button(frame, justify="center", width=4, height=2, padx=0, pady=0, text=start_text, font=("Arial", 12, "bold"), command=lambda row=row, col=column: self.update_num(row, col))
+                    starting_buttons.pack()
+                    starting_buttons["state"] = DISABLED
+                    self.starting_button_dict[(row, column)] = game_buttons
+                    
+                
+                
 
-                self.button_dict[(column, row)] = button
+                
+
+        empty_space = Label(self.window, text="")
+        empty_space.grid(row=10, column=1)
 
         for i in range(1, 10):
             num_button = Button(self.window, width=4, height=2, padx=0, pady=0, text=i, font=("Arial", 12, "bold"), command=lambda i=i: self.set_selected_num(i))
-            num_button.grid(row=10, column=i)
+            num_button.grid(row=11, column=i)
             # lambda i=i means: it stores the value of i at the time your lambda is defined, instead of waiting to look up the value of i later when it will be equal to 9 every time.
 
             self.num_button_dict[i] = num_button
@@ -47,9 +65,34 @@ class Gui:
     def get_selected_num(self):
         return self.selected_num
 
-    def update_num(self, col, row):
+    def update_num(self, row, col):
         num = self.get_selected_num()
-        self.button_dict[(col, row)].config(text=num)
+        self.board.update(num, row, col)
+        self.game_button_dict[(row, col)].config(text=num)
+
+
+class Board:
+    def __init__(self):
+        self.board = [
+                    [0,7,5,0,0,0,0,1,6],
+                    [2,0,0,3,0,0,0,0,0],
+                    [0,0,0,0,0,1,7,0,2],
+                    [0,2,3,1,0,0,4,0,0],
+                    [0,0,0,0,0,7,0,6,0],
+                    [7,0,9,4,0,5,0,0,0],
+                    [5,3,8,0,1,4,9,0,7],
+                    [1,0,7,0,2,8,6,3,4],
+                    [0,0,0,0,9,3,0,5,0]
+                        ]
+            
+    def update(self, num, y, x):
+        self.board[y-1][x-1] = num
+
+    
+    def get_num(self, y, x):
+        return self.board[y-1][x-1]
+
+
 
 
 if __name__ == "__main__":
