c159504531cb3a963cf8891f304f822f7e2e682c
Theo-Coop <theomantelcooper@gmail.com>
2022-10-31 19:06:47 +0000

    windows testing

--- Sudoku/current/windows.py
+++ Sudoku/current/windows.py
@@ -1,5 +1,20 @@
 from tkinter import *
 
-class Opening:
+class Windows:
     def __init__(self):
-        pass
+        self.root = Tk()
+        self.root.title("Hello")
+        self.label = Label(self.root, text="This is the first page").pack()
+        self.but = Button(self.root, text="click or else", command=self.new_window).pack()
+
+        self.root.mainloop()
+
+    def new_window(self):
+        self.root.destroy()
+        self.window = Tk()
+        self.window.title("Page 2 easy")
+        self.new_label = Label(self.window, text="This is page 2").pack()
+
+
+if __name__ == "__main__":
+    Windows()
