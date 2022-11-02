137ef5c965820cdf849d7f4043ca213d04f4ea93
Theo-Coop <theomantelcooper@gmail.com>
2022-11-02 19:22:10 +0000

    actually got windows working - huge if true

--- Sudoku/current/windows.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-
-
-class WindowTemplate:
-    def __init__(self):
-        self.window = Tk()
-
-    def close(self):
-        self.window.destroy()
---
+++ Sudoku/multiple_windows_working.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+
+class WindowTemplate:
+    def __init__(self):
+        self.window = Tk()
+        self.window.geometry("250x250")
+        
+
+    def close(self):
+        self.window.destroy()
+
+
+class hidden(WindowTemplate):
+    def __init__(self):
+        super().__init__()
+        self.label = Label(self.window, text="Hidden").pack()
+        # self.window.withdraw()
+        Window1()
+
+        self.window.mainloop()
+
+        
+
+class Window1(WindowTemplate):
+    def __init__(self):
+        super().__init__()
+        self.label = Label(self.window, text="Page 1").pack()
+
+        self.but = Button(self.window, text="press for page 2", command=self.buttonpress).pack()
+
+
+
+    def buttonpress(self):
+        Window2()
+        self.close()
+
+
+
+class Window2(WindowTemplate):
+    def __init__(self):
+        super().__init__()
+        self.label = Label(self.window, text="Page 2").pack()
+
+        self.but = Button(self.window, text="press for page 1", command=self.buttonpress).pack()
+
+        self.end = Button(self.window, text="Quit", command=self.quit).pack()
+
+
+    def buttonpress(self):
+        Window1()
+        self.close()
+
+    def quit(self):
+        # self.window.quit()
+        exit() # Either or
+
+
+
+hidden()
