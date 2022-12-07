a70d68ca9cbf4d780e508fd8fec3bd09b9c308ad
Theo-Coop <theomantelcooper@gmail.com>
2022-12-07 17:27:37 +0000

    Board generation complete
    With varying difficulties

--- Sudoku/Testing/removing_squares.py
+++ Sudoku/Testing/removing_squares.py
@@ -1,24 +1,11 @@
 import random
 import board_generation_testing
 
-def print_board():
-    for i in range(len(full_board)):
-        if i % 3 == 0 and i != 0:
-            print("- - - - - - - - - - - - ")
-        
-        for j in range(len(full_board[0])):
-            if j % 3 == 0 and j != 0:
-                print(" | ", end="")
-
-            if j == 8:
-                print(full_board[i][j])
-            else:
-                print(str(full_board[i][j]) + " ", end="")
 
 def find_empty(): # find empty space on board
-    for i in range(len(full_board)):
-        for j in range(len(full_board[0])):
-            if full_board[i][j] == 0:
+    for i in range(len(full_board.board)):
+        for j in range(len(full_board.board[0])):
+            if full_board.board[i][j] == 0:
                 return (i, j) # row, column
 
     return False # There are no empty squares
@@ -33,28 +20,46 @@ def solve():
 
     for i in range(1, 10):
         if full_board.num_valid(i, row, col):
-            full_board[row][col] = i
+            full_board.board[row][col] = i
 
-            if solve(full_board):
+            if solve():
                 return True
             else:
-                full_board[row][col] = 0
+                full_board.board[row][col] = 0
 
     return False
+
+
+def remove_square(tuple):
+    r, c = tuple[0], tuple[1]
+    full_board.board[r][c] = 0
+
 
 full_board = board_generation_testing.generatedBoard()
 
 full_board.print_board()
-print(full_board.whole_board_valid())
+
 
 
 dict_nums_to_be_removed = {
-    "easy": (36, 46),
-    "medium": (32, 35),
-    "hard": (28, 31)
+    "easy": (35, 45), # Keep: (36, 46)
+    "medium": (46, 49), # Keep: (32, 35)
+    "hard": (50, 53) # Keep: (28, 31)
 }
 
-difficulty = "easy"
-nums_to_be_removed = random.randint(dict_nums_to_be_removed[difficulty])
 
-print(nums_to_be_removed)
+difficulty = "easy"
+a, b = dict_nums_to_be_removed[difficulty][0], dict_nums_to_be_removed[difficulty][1]
+nums_to_be_removed = random.randint(a,b)
+
+
+cells = [(r, c) for r in range(9) for c in range(9)]
+
+random.shuffle(cells)
+
+
+for i in range(nums_to_be_removed):
+    remove_square(cells[i])
+
+print()
+full_board.print_board()
