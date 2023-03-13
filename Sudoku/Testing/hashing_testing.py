fd87a7f512fef620382b17eb483e8f765dada257
Theo-Coop <theomantelcooper@gmail.com>
2023-03-13 20:23:50 +0000

    yes

--- Sudoku/Testing/hashing_testing.py
+++ Sudoku/Testing/hashing_testing.py
@@ -1,32 +1,48 @@
+# import bcrypt
+
+
+# users = {}
+
+# username = "TheoMC"
+# test = "gabecharlie123!easy"
+
+# password = test.encode()
+# salt = bcrypt.gensalt()
+
+# hashed = bcrypt.hashpw(password, salt)
+
+# users[username] = hashed
+
+
+
+
+# new_user = "TheoMC"
+# new_password_str = "gabecharlie123!easy"
+# new_password = new_password_str.encode()
+
+
+# hash = users[username]
+
+# if bcrypt.checkpw(new_password, hash):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# print(users)
+
+
+
+
+
 import bcrypt
 
+start_text = b"abcde123"
 
-users = {}
-
-username = "TheoMC"
-test = "gabecharlie123!easy"
-
-password = test.encode()
 salt = bcrypt.gensalt()
+print(salt)
 
-hashed = bcrypt.hashpw(password, salt)
-
-users[username] = hashed
-
+hash = bcrypt.hashpw(start_text, salt)
 
 
-
-new_user = "TheoMC"
-new_password_str = "gabecharlie123!easy"
-new_password = new_password_str.encode()
-
-
-hash = users[username]
-
-if bcrypt.checkpw(new_password, hash):
-    print("Yes")
-else:
-    print("No")
-
-
-print(users)
+print(hash)
